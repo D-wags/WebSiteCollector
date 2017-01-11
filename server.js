68265e52ab28423bddb64adb25f440a404413d81
Drew Wagner <drew_wagner@utexas.edu>
2017-01-11 16:37:34 -0600

    Delte route working

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+
+
 var MongoClient = require('mongodb').MongoClient;
 
 var app = express();
 
 app.set('view engine', 'ejs');
+
+app.use(methodOverride('_method'));
 
 var db;
 
@@ -13,13 +19,14 @@ MongoClient.connect('mongodb://swagner:Hippo667!@ds157078.mlab.com:57078/infosto
   db = database;
 
   app.listen(3000, function() {
-    console.log('Shit be istenin on 3000')
+    console.log('Shit be listenin on 3000')
   })
 })
 
 
 //plug bodyparser into app to read form
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
 	// res.sendFile(__dirname + "/index.html");
@@ -47,7 +54,15 @@ app.post("/websites", function(req, res) {
 	});
 });
 
-
-
-
-
+app.delete("/websites/:name", function(req, res){
+	console.log("???");
+	console.log(req.params.name);
+	var item = req.params.name;
+	res.redirect('/');
+	db.collection('websites').findOneAndDelete({name: item},
+		function(err, result) {
+			if (err) return res.send(500, err);
+			console.log("DELETED");
+			res.send("DELETED BRO!");
+		});
+});
